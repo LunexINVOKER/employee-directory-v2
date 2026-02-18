dac5822b49b9af5eab169f9c9132d71ab4033777
LunexINVOKER <karapuz2209@gmail.com>
2026-02-17 23:21:08 -0500

    completed

---
+++ routes/employees.js
@@ -0,0 +1,39 @@
+import express from "express";
+import employees from "#db/employees";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send(employees);
+});
+
+router.get("/random", (req, res) => {
+  const randomIndex = Math.floor(Math.random() * employees.length);
+  res.send(employees[randomIndex]);
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const employee = employees.find((e) => e.id === +id);
+
+  if (!employee) {
+    return res.status(404).send("Employee not found");
+  }
+
+  res.send(employee);
+});
+
+router.post("/", (req, res) => {
+  if (!req.body || !req.body.name) {
+    return res.status(400).send("A valid name is required.");
+  }
+
+  const { name } = req.body;
+
+  const id = employees.length + 1;
+  const newEmployee = { id, name };
+  employees.push(newEmployee);
+  res.status(201).send(newEmployee);
+});
+
+export default router;
